9078a64c8e7a40faaef80d1ef82b7829f8ef3ebb
sudongyu <976499226@qq.com>
2022-04-16 00:08:29 +0800

    chore: 完全搞懂esm和require区别

--- es6bar.js
+++ es6bar.js
@@ -1,8 +1,21 @@
 // const { getFullName, getSurname } = require("./foo");
 import { getFullName, getSurname } from "./es6foo.js";
+import './es6foo1.js'
+
+function asyncFunction(){
+  return new Promise((resolve,reject)=>{
+    setTimeout(()=>{
+      resolve('sudongyu')
+    },3000)
+  })
+}
+
+const a= await asyncFunction()
+console.log(a);
 
 console.log(
   "getFullName",
   getFullName("monster", "lessons"),
   getSurname("foo", "bar")
 );
+
--- es6foo.js
+++ es6foo.js
@@ -1,3 +1,14 @@
+function asyncFunction(){
+  return new Promise((resolve,reject)=>{
+    setTimeout(()=>{
+      resolve('hello')
+    },3000)
+  })
+}
+
+const a= await asyncFunction()
+console.log(a);
+
 export const getFullName = (name, surname) => {
   return name + " " + surname;
 };
